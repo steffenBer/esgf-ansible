83d2401cdd9843a44ffb276386536bb4659e31e6
skremdwd <steffen.kremer@dwd.de>
2021-10-15 08:51:48 +0000

    Globus cert removed because it is not needed

--- prepare_dwd.sh
+++ prepare_dwd.sh
@@ -4,6 +4,5 @@ set -e
 groupadd myproxy
 # Certificate fix
 cp aims1.llns_chain.pem /etc/pki/ca-trust/source/anchors/aims1.llns_chain.pem
-cp api.globus_chain.pem /etc/pki/ca-trust/source/anchors/api.globus_chain.pem
 update-ca-trust
 echo Done!
