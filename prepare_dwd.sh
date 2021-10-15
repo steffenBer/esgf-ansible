42a44209907f9043b42139e8a2ed970ab8f10ebf
skremdwd <steffen.kremer@dwd.de>
2021-10-15 09:26:01 +0000

    Added automated backup of the trust store, because it can get corrupted very easily

--- prepare_dwd.sh
+++ prepare_dwd.sh
@@ -5,4 +5,6 @@ groupadd myproxy
 # Certificate fix
 cp aims1.llns_chain.pem /etc/pki/ca-trust/source/anchors/aims1.llns_chain.pem
 update-ca-trust
+# Create backup of trust store
+cp /etc/pki/tls/certs/ca-bundle.crt /etc/pki/tls/certs/ca-bundle.crt_bak
 echo Done!
